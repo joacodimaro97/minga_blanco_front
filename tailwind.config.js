e5dafadfbf6d228feb1c1bf04f5ceb506b164917
Joaquin Dimaro <joacodimaro97@gmail.com>
2023-04-24 16:11:15 -0300

    correccion footer2

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,6 @@ module.exports = {
   extend: {
     backgroundImage: {
       'hero': "url('./assets/images/1mangashero.png')",
-      'footerimg': "url('./assets/images/Footer.png')",
       'fondo': "('./assets/images/narutofon.png')"
 
     },
