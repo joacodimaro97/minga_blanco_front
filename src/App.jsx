73d01a313d7980daba63e379a0a659e112472a15
Joaquin Dimaro <joacodimaro97@gmail.com>
2023-04-24 23:55:50 -0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import apiUrl from '../api'
-import Footer from './components/footer';
-import Navbar from './components/navbar';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
 import Index from './pages/Index';
 function App() {
 
