eadc3538fd26a83fd6b7f7685d38d03af9fb901b
CrisDev <alvarezmartinez132@gmail.com>
2023-04-28 17:24:02 -0500

    m-02-01_5:23

--- src/components/AuthorForm.jsx
+++ src/components/AuthorForm.jsx
@@ -4,7 +4,7 @@ import NavBar from './Navbar'
 const AuthorForm = () => {
   return (
     <>
-    <NavBar className="bg-white"/>
+    <div>Hola Mundo!!!</div>
     </>
   )
 }
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom'
 import Main from '../App.jsx'
 import Layout from '../layouts/Main.jsx'
 import ChapterForm from '../pages/ChapterForm.jsx'
+import AuthorForm from '../components/AuthorForm.jsx'
 
 
 const routes = createBrowserRouter([
@@ -10,9 +11,9 @@ const routes = createBrowserRouter([
     children:[
         { path: '/', element:<Main/>, errorElement:<div>ups hubo un error</div>}, //aca es es el home
         { path: '/chapter-form', element:<ChapterForm/>},
-        
-    ]
-    }
+        ]
+    },
+    { path: '/author-form', element:<AuthorForm/>}
 ])
 
 export default routes
