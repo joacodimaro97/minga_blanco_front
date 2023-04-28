35f5a6843fcd205ae7524fd037365e1ed974d6cf
unknown <sebastianovalle24@gmail.com>
2023-04-28 18:34:49 -0300

    agregue router

---
+++ src/pages/ChapterForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ChapterForm() {
+  return (
+    <>
+    <div className='w-6/12 h-screen bg-white'> <p className='text-black'> DSFSDFKLASNKLSDNSDKFNSADKFJNSAFSDFS</p></div>
+    </>
+  )
+}
